6dceab10ee35684acf3791c23a19562d07eec933
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2023-12-17 16:06:22 +0100

    Button 1-9

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,13 +7,68 @@
 
 import SwiftUI
 
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "x"
+    case equal = "="
+    case clear  = "C"
+    case clearAll  = "AC"
+    case decimal  = ","
+    case negative  = "-/+"
+}
+
 struct ContentView: View {
+    
+    let buttons: [[CalcButton]] = [
+        [.seven, .eight, .nine],
+        [.four, .five, .six],
+        [.one, .two, .three],]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            //display Text
+            HStack {
+                Spacer()
+                Text("0")
+                    .font(.system(size: 52))
+                    .bold()
+            }
+            .padding()
+            
+            //display Buttons
+            
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
+                                   label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 30))
+                                    .frame(width: 70, height: 70)
+                                    .background(Color.orange)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            })
+                            
+                        }
+                    }
+                }
+            
+            
+            
         }
         .padding()
     }
